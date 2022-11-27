341344d7f8d1a6824fab20541bccfd4b89225980
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-27 23:37:24 +0530

    addeddequeue.java file

---
+++ dequeue.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+
+// A LL node to store queue entry
+class QNode{
+    int key;
+    QNode next;
+    public QNode(int key){
+        this.key = key;
+        this.next = null;
+    }
+}
+
+public class dequeue{
+    QNode front, rear;
+    public dequeue(){
+        this.front = this.rear = null;
+    }
+    // method to add an key to queue
+    void enqueue(int key) {
+        // craete a new LL node
+        QNode temp = new QNode(key);
+        if(this.rear == null){
+            this.front = this.rear = temp;
+            return;
+        }
+        // add the new node at end and change to rear
+        this.rear.next = temp;
+        this.rear = temp;
+
+    }
+    void dequeue(){
+        if(this.front==null)
+             return;
+        // store previous front and move front one node
+        QNode temp = this.front;
+        this.front = this.front.next;
+        //if front becomes null, then change raer also null
+        if(this.front==null)
+              this.rear = null;
+    }
+
+    
+    public static void main(String args[]){
+        dequeue q = new dequeue();
+        q.enqueue(56);
+        q.enqueue(30);
+        q.enqueue(70);
+        q.dequeue();
+        System.out.println(q.front.key);
+        q.dequeue();
+        System.out.println(q.front.key);
+
+        
+
+    }
+    
+}
