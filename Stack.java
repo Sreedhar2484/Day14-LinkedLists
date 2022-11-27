29c3885cea1b211a54f3a52c362c33aa766c1ab9
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-27 23:31:06 +0530

    addedStack.java file

---
+++ Stack.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stack{
+    public static void main(String args[]){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        System.out.println(list);
+    }
+}
